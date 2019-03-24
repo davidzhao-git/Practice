972905e7bc476747f1d41cf3662f9c8ce928fa19
David Zhao <davidzhao2@gmail.com>
2019-03-24 15:19:51 -0400

    modified insert

--- linked_list_insert.cpp
+++ linked_list_insert.cpp
@@ -12,45 +12,88 @@ class SinglyLinkedListNode {
         }
 };
 
+// insert node at the tail of the list 
 SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
 
+    SinglyLinkedListNode *temp = new SinglyLinkedListNode(data);
     if (head == nullptr) {
+        head = temp;
         return head;
     }
 
-    SinglyLinkedListNode *temp = new SinglyLinkedListNode(data);
+    // SinglyLinkedListNode *temp = new SinglyLinkedListNode(data);
     SinglyLinkedListNode *front = head;
 
     while (front->next != nullptr) {
         front = front->next;
     }
     front->next = temp;
-
+    
+    /* front = head;
+    while (front != nullptr) {
+        cout << front->data;
+        front = front->next;
+    } 
+    */
+    
     return head;
 }
 
+// insert node at the head of the list 
 SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
 
+    SinglyLinkedListNode *temp = new SinglyLinkedListNode(data);
+
     if (llist == nullptr) {
+        llist = temp;
         return llist;
     }
-
-    SinglyLinkedListNode *temp = new SinglyLinkedListNode(data);
+    
     temp->next = llist;
     llist = temp;
-
     return llist;
 }
 
-int main() {
-    SinglyLinkedListNode *node = new SinglyLinkedListNode(10);
-    node = insertNodeAtHead(node, 20);
-    node = insertNodeAtHead(node, 4);
-    node = insertNodeAtTail(node, 77);
-    node = insertNodeAtTail(node, 63);
+// insert node at a given position 
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+    
+    if (head == nullptr) {
+        return head;
+    }
+    
+    SinglyLinkedListNode *new_node = new SinglyLinkedListNode(data);
+    SinglyLinkedListNode *front = head;
+    int i = 0;
+    while (i < position-1) {
+        front = front->next;
+        i++;
+    }
+    SinglyLinkedListNode *temp = front->next;
+    front->next = new_node;
+    new_node->next = temp;
+    
+    return head;
+}
 
+int main() {
+    SinglyLinkedListNode *node = new SinglyLinkedListNode(16);
+    // node = insertNodeAtHead(node, 20);
+    // node = insertNodeAtHead(node, 4);
+    node = insertNodeAtTail(node, 13);
+    node = insertNodeAtTail(node, 7);
+    node = insertNodeAtTail(node, 20);
+    node = insertNodeAtTail(node, 30);
+    node = insertNodeAtPosition(node, 1, 2);
+    
     while (node != nullptr) {
-        cout << node->data << endl;
+        cout << node->data << " ";
         node = node->next;
     }
+    cout << endl;
+    
+    node = deleteNode(node, 3);
 }
+
+
+
+
